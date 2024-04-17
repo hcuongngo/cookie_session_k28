193781a2e9e7da3d9c3020fa62fe805da3855a34
huycuongngo <cuong.nh@softenmind.com>
2024-04-17 19:31:06 +0700

    update api

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const url = require('url')
 const bcrypt = require('bcrypt')
-const cryto = require('cryto')
+const crypto = require('crypto')
 
 const salRound = 10;
 
@@ -38,7 +38,7 @@ const sessions = {}
 
 const generateSessionId = () => {
   // uuidv4
-  return cryto.randomBytes(16).toString('hex')
+  return crypto.randomBytes(16).toString('hex')
 }
 
 const handleAPIRegister = (req, res) => {
@@ -47,8 +47,268 @@ const handleAPIRegister = (req, res) => {
     body += chunk.toString()
   })
 
-  req.on("end", () => {
+  req.on("end", async () => {
     const params = JSON.parse(body)
-    const { email, password, role } = params
+    const { email, password } = params
+    if (!email) {
+      res.writeHead(400, { 'Content-Type': 'text/plain' })
+      res.end("Email is required")
+      return
+    }
+    if (!password) {
+      res.writeHead(400, { "Content-Type": "text/plain" })
+      res.end("Password is required")
+      return
+    }
+    const newUser = { id: users.length + 1, email, password, role: "user" }
+    newUser.password = await hashPassword(password)
+    users.push(newUser)
+    console.log("users after push", users)
+    const cloneNewUser = { ...newUser }
+    delete cloneNewUser.password
+    res.writeHead(201, { "Content-Type": "application/json" })
+    res.end(JSON.stringify({
+      message: "Register Success",
+      data: cloneNewUser
+    }))
   })
 }
+
+const handleAPILogin = (req, res) => {
+  let body = ''
+  req.on("data", (chunk) => {
+    body += chunk.toString()
+  })
+  req.on("end", async () => {
+    const params = JSON.parse(body)
+    const { email, password } = params
+    if (!email) {
+      res.writeHead(400, { 'Content-Type': 'text/plain' })
+      res.end("Email is required")
+      return
+    }
+    if (!password) {
+      res.writeHead(400, {
+        "Content-Type": "text/plain"
+      })
+      res.end("Password is required")
+      return
+    }
+    const checkEmailUser = users.find(user => user.email === email)
+    if (!checkEmailUser) {
+      res.writeHead(401, { "Content-Type": "text/plain" })
+      res.end("Unauthorized")
+      return
+    }
+    const checkPasswordUser = await comparePassword(password, checkEmailUser.password)
+    if (!checkPasswordUser) {
+      res.writeHead(401, { "Content-Type": "text/plain" })
+      res.end("Unauthorized")
+      return
+    }
+    console.log("Object.values", Object.values(sessions))
+    const existingSession = Object.values(sessions).find(session => session.email === email)
+    if (existingSession) {
+      for (const sessionId in sessions) {
+        const session = sessions[sessionId]
+        if (session.email === email) {
+          console.log("existingSession sessionId", sessionId)
+          res.setHeader('Set-Cookie', `sessionId=${sessionId}; HttpOnly`)
+        }
+      }
+    } else {
+      const sessionId = generateSessionId()
+      sessions[sessionId] = checkEmailUser
+      console.log("sessions", sessions)
+      res.setHeader('Set-Cookie', `sessionId=${sessionId}; HttpOnly`)
+    }
+    const cloneUser = { ...checkEmailUser }
+    delete cloneUser.password
+    res.writeHead(200, { "Content-Type": "application/json" })
+    res.end(JSON.stringify({
+      message: "Login Success",
+      data: cloneUser
+    }))
+  })
+}
+
+const handleAPIChangePassword = (req, res) => {
+  let body = ''
+  req.on('data', (chunk) => {
+    body += chunk.toString()
+  })
+  req.on("end", async () => {
+    console.log("cookie", req.headers.cookie)
+    const sessionId = req.headers.cookie && req.headers.cookie.split("; ").find(cookie => cookie.startsWith("sessionId=")).split("=")[1]
+    console.log("sessionId", sessionId)
+    if (!sessionId || !sessions[sessionId]) {
+      res.writeHead(401, { "Content-Type": "text/plain" })
+      res.end("Unauthorized")
+      return
+    }
+    const params = JSON.parse(body)
+    const { email, password, newPassword } = params
+    if (!email) {
+      res.writeHead(400, { 'Content-Type': 'text/plain' })
+      res.end("Email is required")
+      return
+    }
+    if (!password) {
+      res.writeHead(400, {
+        "Content-Type": "text/plain"
+      })
+      res.end("Password is required")
+      return
+    }
+    if (!newPassword) {
+      res.writeHead(400, {
+        "Content-Type": "text/plain"
+      })
+      res.end("New password is required")
+      return
+    }
+    const checkEmailUser = users.find(user => user.email === email)
+    if (!checkEmailUser) {
+      res.writeHead(401, { "Content-Type": "text/plain" })
+      res.end("Unauthorized")
+      return
+    }
+    const checkPasswordUser = await comparePassword(password, checkEmailUser.password)
+    if (!checkPasswordUser) {
+      res.writeHead(401, { "Content-Type": "text/plain" })
+      res.end("Unauthorized")
+      return
+    }
+    const hashedNewPassword = await hashPassword(newPassword)
+    checkEmailUser.password = hashedNewPassword
+    sessions[sessionId].password = hashedNewPassword
+    console.log("users", users)
+    console.log("sessions", sessions)
+    res.writeHead(200, {
+      "Content-Type": "text/plain"
+    })
+    res.end("Change password success")
+  })
+}
+
+const handleAPIForgotPassword = (req, res) => {
+  let body = ''
+  req.on("data", (chunk) => {
+    body += chunk.toString()
+  })
+  req.on("end", async () => {
+    const params = JSON.parse(body)
+    const { email, newPassword } = params
+    if (!email) {
+      res.writeHead(400, {
+        "Content-Type": "text/plain"
+      })
+      res.end("Email is required")
+      return
+    }
+    if (!newPassword) {
+      res.writeHead(400, {
+        "Content-Type": "text/plain"
+      })
+      res.end("New password is required")
+      return 
+    }
+    const checkEmailUser = users.find(user => user.email === email)
+    if (!checkEmailUser) {
+      res.writeHead(401, {
+        "Content-Type": "text/plain"
+      })
+      res.end("Unauthorized")
+      return
+    }
+
+    checkEmailUser.password = await hashPassword(newPassword)
+    res.writeHead(200, {
+      "Content-Type": "text/plain"
+    })
+    res.end("Reset password success")
+  })
+}
+
+const handleApiLogout = (req, res) => {
+  const sessionId = req.headers.cookie && req.headers.cookie.split("; ").find(item => item.startsWith("sessionId=")).split("=")[1]
+  console.log("sessionId", sessionId)
+  if (!sessionId || !sessions[sessionId]) {
+    res.writeHead(401, {
+      "Content-Type": "text/plain"
+    })
+    res.end("Unauthorized")
+    return
+  }
+  delete sessions[sessionId]
+  console.log("after delete", sessions)
+  res.writeHead(200, {
+    "Content-Type": "text/plain"
+  })
+  res.end("Logout")
+}
+
+
+const handleRequest = (req, res) => {
+  const reqUrl = url.parse(req.url, true)
+  const path = reqUrl.pathname
+  console.log("path", path)
+  const method = req.method
+  console.log("method", method)
+  if (method === "POST" && path === "/api/auth/register") {
+    handleAPIRegister(req, res)
+  } else if (method === "POST" && path === "/api/auth/login") {
+    handleAPILogin(req, res)
+  } else if (method === "PUT" && path === "/api/auth/change-password") {
+    handleAPIChangePassword(req, res)
+  } else if (method === "PUT" && path === "/api/auth/forgot-password") {
+    handleAPIForgotPassword(req, res)
+  }
+  else {
+    res.writeHead(404, { "Content-Type": "text/plain" })
+    res.end("Not Found")
+  }
+}
+
+const server = http.createServer(handleRequest)
+
+const PORT = 3000
+server.listen(PORT, () => {
+  console.log(`Running on ${PORT}`)
+})
+
+
+// const person = {
+//   name: "a",
+//   age: 20
+// }
+
+// const sessions = {
+//   "1cd32818d1374bd555a7cc20018c50ff": {
+//     id: 3,
+//     email: 'user3@gmail.com',
+//     password: '$2b$10$tT3MGA73HGE9wuGagl4/NOZ21TSNw0fAn7KsoPmZ5bbpUU.4rgr2m',
+//     role: 'user'
+//   },
+//   '58e77a4aed850104116d1804bb7d8f08': {
+//     id: 3,
+//     email: 'user3@gmail.com',
+//     password: '$2b$10$tT3MGA73HGE9wuGagl4/NOZ21TSNw0fAn7KsoPmZ5bbpUU.4rgr2m',
+//     role: 'user'
+//   },
+// }
+// Object.values(sessions)
+// [
+//  {
+//     id: 3,
+//     email: 'user3@gmail.com',
+//     password: '$2b$10$tT3MGA73HGE9wuGagl4/NOZ21TSNw0fAn7KsoPmZ5bbpUU.4rgr2m',
+//     role: 'user'
+//   },
+//   {
+//     id: 3,
+//     email: 'user3@gmail.com',
+//     password: '$2b$10$tT3MGA73HGE9wuGagl4/NOZ21TSNw0fAn7KsoPmZ5bbpUU.4rgr2m',
+//     role: 'user'
+//   }
+// ]
