7073ac886a5fd88859e375b4a81f60c87b982d38
huycuongngo <cuong.nh@softenmind.com>
2024-05-08 19:23:21 +0700

    api creat and update item

--- cookie_expiretime.js
+++ cookie_expiretime.js
@@ -48,7 +48,7 @@ const handleApiRegister = (req, res) => {
   req.on('data', (chunk) => {
     body += chunk.toString()
   })
-  
+
   req.on("end", async () => {
     const params = JSON.parse(body)
     const { email, password } = params
@@ -68,7 +68,7 @@ const handleApiRegister = (req, res) => {
 }
 
 const handleApiLogin = (req, res) => {
-  let body = ''  
+  let body = ''
   req.on('data', (chunk) => {
     body += chunk.toString()
   })
@@ -142,6 +142,7 @@ const handleApiChangePassword = (req, res) => {
       return
     }
     if (parseInt(expiredTime) < moment().unix()) {
+      delete sessions[sessionId]
       res.writeHead(401, {
         "Content-Type": "text/plain"
       })
@@ -175,6 +176,217 @@ const handleApiChangePassword = (req, res) => {
   })
 }
 
+const handleApiForgotPassword = (req, res) => {
+  let body = ""
+  req.on("data", (chunk) => {
+    body += chunk.toString()
+  })
+  req.on("end", async () => {
+    const { email, newPassword } = JSON.parse(body)
+    const checkEmailUser = users.find(user => user.email === email)
+    if (!checkEmailUser) {
+      res.writeHead(401, {
+        "Content-Type": "text/plain"
+      })
+      res.end("Unauthorized")
+      return
+    }
+    const hashedNewPassword = await hashPassword(newPassword)
+    checkEmailUser.password = hashedNewPassword
+    res.writeHead(200, {
+      "Content-Type": "text/plain"
+    })
+    res.end("Reset password success")
+  })
+}
+
+const handleApiLogout = (req, res) => {
+  const sessionId = req.headers.cookie && req.headers.cookie.split("; ").find(cookie => cookie.startsWith("sessionId=")).split("=")[1]
+  if (!sessionId || !sessions[sessionId]) {
+    res.writeHead(401, {
+      "Content-Type": "text/plain"
+    })
+    res.end("Unauthorized")
+    return
+  }
+  delete sessions[sessionId]
+  res.writeHead(200, {
+    "Content-Type": "text/plain"
+  })
+  res.end("Logout")
+}
+
+const handleApiGetItems = (req, res) => {
+  const sessionId = req.headers.cookie && req.headers.cookie.split("; ").find(cookie => cookie.startsWith("sessionId=")).split("=")[1]
+  const expiredTime = req.headers.cookie && req.headers.cookie.split("; ").find(cookie => cookie.startsWith("Expires=")).split("=")[1]
+  if (!sessionId || !sessions[sessionId]) {
+    res.writeHead(401, {
+      "Content-Type": "text/plain"
+    })
+    res.end("Unauthorized")
+    return
+  }
+  if (parseInt(expiredTime) < moment().unix()) {
+    delete sessions[sessionId]
+    res.writeHead(401, {
+      "Content-Type": "text/plain"
+    })
+    res.end("Cookie expired. Login again")
+    return
+  }
+  res.writeHead(200, {
+    "Content-Type": "application/json"
+  })
+  res.end(JSON.stringify({
+    message: "Get items success",
+    data: items
+  }))
+}
+
+const handleApiGetItemById = (req, res) => {
+  const sessionId = req.headers.cookie && req.headers.cookie.split("; ").find(cookie => cookie.startsWith("sessionId=")).split("=")[1]
+  if (!sessionId || !sessions[sessionId]) {
+    res.writeHead(401, {
+      "Content-Type": "text/plain"
+    })
+    res.end("Unauthorized")
+    return
+  }
+  const expiredTime = req.headers.cookie && req.headers.cookie.split("; ").find(cookie => cookie.startsWith("Expires=")).split("=")[1]
+  if (parseInt(expiredTime) < moment().unix()) {
+    delete sessions[sessionId]
+    res.writeHead(401, {
+      "Content-Type": "text/plain"
+    })
+    res.end("Cookie expired. Login again")
+    return
+  }
+  const reqUrl = url.parse(req.url, true)
+  const path = reqUrl.pathname
+  const itemId = parseInt(path.split("/")[3])
+  console.log({ itemId })
+  const index = items.findIndex(item => item.id === itemId)
+  if (index === -1) {
+    res.writeHead(404, {
+      "Content-Type": "text/plain"
+    })
+    res.end("Item id not found")
+    return
+  }
+  res.writeHead(200, {
+    "Content-Type": "application/json"
+  })
+  res.end(JSON.stringify({
+    message: "Get item detail success",
+    data: items[index]
+  }))
+
+}
+
+const handleApiGetItemsPagination = (req, res) => {
+  const sessionId = req.headers.cookie && req.headers.cookie.split("; ").find(cookie => cookie.startsWith("sessionId=")).split("=")[1]
+  if (!sessionId || !sessions[sessionId]) {
+    res.writeHead(401, {
+      "Content-Type": "text/plain"
+    })
+    res.end("Unauthorized")
+    return
+  }
+  const expiredTime = req.headers.cookie && req.headers.cookie.split("; ").find(cookie => cookie.startsWith("Expires=")).split("=")[1]
+  if (parseInt(expiredTime) < moment().unix()) {
+    delete sessions[sessionId]
+    res.writeHead(401, {
+      "Content-Type": "text/plain"
+    })
+    res.end("Cookie expired. Login again")
+    return
+  }
+  const reqUrl = url.parse(req.url, true)
+  const path = reqUrl.pathname
+  const pageIndex = reqUrl.query.pageIndex || 1
+  const limit = reqUrl.query.limit || 10
+  const startIndex = (pageIndex - 1) * limit
+  const endIndex = startIndex + limit - 1
+  let result = {
+    data: items.slice(startIndex, endIndex + 1),
+    itemPerPage: limit,
+    totalPages: Math.ceil(items.length / limit),
+    currentPage: pageIndex,
+  }
+  res.writeHead(200, {
+    "Content-Type": "application/json"
+  })
+  res.end(JSON.stringify({
+    message: "Get items pagination success",
+    data: result
+  }))
+}
+
+const handleApiCreateNewItem = (req, res) => {
+  const sessionId = req.headers.cookie && req.headers.cookie.split("; ").find(cookie => cookie.startsWith("sessionId=")).split("=")[1]
+  if (!sessionId || !sessions[sessionId]) {
+    res.writeHead(401, {
+      "Content-Type": "text/plain"
+    })
+    res.end("Unauthorized")
+    return
+  }
+  const expiredTime = req.headers.cookie && req.headers.cookie.split("; ").find(cookie => cookie.startsWith("Expires=")).split("=")[1]
+  if (parseInt(expiredTime) < moment().unix()) {
+    delete sessions[sessionId]
+    res.writeHead(401, {
+      "Content-Type": "text/plain"
+    })
+    res.end("Cookie expired. Login again")
+    return
+  }
+  if (sessions[sessionId].role !== "admin") {
+    res.writeHead(403, {
+      "Content-Type": "text/plain"
+    })
+    res.end("Forbidden")
+    return
+  }
+  let body = "";
+  req.on("data", (chunk) => {
+    body += chunk.toString()
+    console.log("body", body)
+  })
+  req.on("end", () => {
+    let newItem = JSON.parse(body)
+    console.log("newItem", newItem)
+    const { name, description } = newItem
+    if (!name) {
+      res.writeHead(400, {
+        "Content-Type": "text/plain"
+      })
+      res.end("Name is required")
+      return
+    }
+    if (!description) {
+      res.writeHead(400, {
+        "Content-Type": "text/plain"
+      })
+      res.end("Description is required")
+      return 
+    }
+    newItem = { id: items.length + 1, ...newItem }
+    items.push(newItem)
+    res.writeHead(201, {
+      "Content-Type": "application/json"
+    })
+    res.end(JSON.stringify({
+      message: "Create new item success",
+      data: newItem
+    }))
+  })
+}
+
+const handleApiUpdateItem = (req, res) => {
+  const sessionId = req.headers.cookie && req.headers.cookie.split("; ").find(cookie => cookie.startsWith("sessionId=")).split("=")[1]
+  
+}
+
 const handleRequest = (req, res) => {
   const reqUrl = url.parse(req.url, true)
   const path = reqUrl.pathname
@@ -184,6 +396,20 @@ const handleRequest = (req, res) => {
     handleApiRegister(req, res)
   } else if (req.method === "POST" && path === "/api/auth/login") {
     handleApiLogin(req, res)
+  } else if (req.method === "PUT" && path === "/api/auth/change-password") {
+    handleApiChangePassword(req, res)
+  } else if (req.method === "PUT" && path === "/api/auth/forgot-password") {
+    handleApiForgotPassword(req, res)
+  } else if (req.method === "POST" && path === "/api/auth/logout") {
+    handleApiLogout(req, res)
+  } else if (req.method === "GET" && path === "/api/items") {
+    handleApiGetItems(req, res)
+  } else if (req.method === "GET" && path.startsWith("/api/items/") && itemId) {
+    handleApiGetItemById(req, res)
+  } else if (req.method === "GET" && path === "/api/items/pagination") {
+    handleApiGetItemsPagination(req, res)
+  } else if (req.method === "POST" && path === "/api/items") {
+    handleApiCreateNewItem(req, res)
   }
   else {
     res.writeHead(404, {
